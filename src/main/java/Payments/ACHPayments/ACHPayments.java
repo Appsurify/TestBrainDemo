c6887245fe87babe865f01f6d379c21e6fd82715
James Farrier <jamesfarrier@gmail.com>
2021-03-24 17:37:59 +1300

    Update ACH Payments for call

--- src/main/java/Payments/ACHPayments/ACHPayments.java
+++ src/main/java/Payments/ACHPayments/ACHPayments.java
@@ -6,7 +6,7 @@ import Payments.*;
  * Created by james on 24/05/2019.
  */
 public class ACHPayments {
-    int i=2;
+    int i=3;
     public String dummyMethod(String input){
         return input;
     }
